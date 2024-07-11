3eb2b20564e5d46d9b92a7cf1e6fce7dc1faf5d6
Lanvis Wei <lanvis.wei@gmail.com>
2024-07-11 09:04:16 +0800

    modify

--- 2024_07_09/main.py
+++ 2024_07_09/main.py
@@ -3,5 +3,18 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+def index():
+    return render_template('index.html.jinja')
+
+@app.route('/new')
+def new():
+    return render_template('new.html.jinja')
+
+@app.route('/youbike')
+def youbike():
+    return render_template('youbike.html.jinja')
+
+@app.route('/contact')
+def contact():
+    # show the post with the given id, the id is an integer
+    return render_template('contact.html.jinja')
