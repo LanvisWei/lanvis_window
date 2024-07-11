a6e7a648ed68ad00979411e386e64ce5276e880f
Lanvis Wei <lanvis.wei@gmail.com>
2024-07-11 16:35:49 +0800

    modify

--- 2024_07_11/main.py
+++ 2024_07_11/main.py
@@ -11,12 +11,13 @@ def index():
     # print(list(map(lambda value:value[0],data.get_areas())))
     selected_area = request.args.get('area')
     areas = [tup[0] for tup in data.get_areas()]
-    if selected_area is None:
-        print('首插')
+    # if selected_area is None:
+    selected_area='士林區' if selected_area is None else selected_area
+        # print('首插')
         # return render_template('index.html.jinja', areas=areas)
-        return render_template('index.html.jinja', areas=areas)
+    return render_template('index.html.jinja', areas=areas, area='士林區')
     # print(areas)
     # return render_template('index.html.jinja', areas=areas)
-    else:
-        print(selected_area)
-        return render_template('index.html.jinja', areas=areas)
+    # else:
+    #     # print(selected_area)
+    #     return render_template('index.html.jinja', areas=area, show_area=selected_area)
