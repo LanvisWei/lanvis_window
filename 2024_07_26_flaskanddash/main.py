d9a4aee673a57e9da892d97518f8b41354f7ff0c
Lanvis Wei <lanvis.wei@gmail.com>
2024-07-26 11:49:42 +0800

    modify

--- 2024_07_26_flaskanddash/main.py
+++ 2024_07_26_flaskanddash/main.py
@@ -1,7 +1,16 @@
 from flask import Flask,render_template,request
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.serving import run_simple
+from dashboard.board1 import app1
+from dashboard.board2 import app2
 import data
 
 app = Flask(__name__)
+application = DispatcherMiddleware(app,{
+    "/dashboard/app1":app1.server,
+    "/dashboard/app2":app2.server
+})
+
 @app.route("/")
 def index():
     return render_template("index.html.jinja")
@@ -20,4 +29,7 @@ def index1():
     return render_template('index1.html.jinja',areas=areas,show_area=selected_area,detail_snaes=detail_snaes)    
     
     
-    
+
+
+if __name__ == "__main__":
+    run_simple("localhost", 8080, application,use_debugger=True,use_reloader=True)
