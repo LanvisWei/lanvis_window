a8a35b9b362ee842c2022af4fe434607378a1c90
Lanvis Wei <lanvis.wei@gmail.com>
2024-07-30 13:46:37 +0800

    modify

--- 2024_07_30_flashanddash/auth/main.py
+++ 2024_07_30_flashanddash/auth/main.py
@@ -2,7 +2,7 @@ from flask import Blueprint,render_template
 
 auth_blueprint = Blueprint('auth',__name__)
 
-@auth_blueprint.route("/auth/")
-@auth_blueprint.route("/auth/login")
+@auth_blueprint.route("/auth/",methods=["get","post"])
+@auth_blueprint.route("/auth/login",methods=["get","post"])
 def index():
     return render_template('/auth/login.html.jinja')
